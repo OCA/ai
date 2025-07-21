6364bb0c0746af2ffd0fe717b020a6db7194fca3
qappsuy <juan@qapps.io>
2025-07-21 15:27:13 -0300

    [ADD] polpo_vapi_integration: initial commit

---
+++ polpo_vapi_integration/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Odoo Vapi Call Integration',
+    'summary': 'Integrate Odoo with Vapi.ai for voice agent call logging and management.',
+    'description': 'This module enables seamless integration between Odoo and Vapi.ai, allowing automated logging and management of calls handled by AI voice agents. It provides call logs, settings, and user-specific API keys.',
+    'version': '16.0.1.0.0',
+    'author': 'Juan Puig, Polpo.uy',
+    'maintainers': ['qappsuy'],
+    'website': 'https://polpo.uy',
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web'],
+    'icon': '/polpo_vapi_integration/static/description/icon.png',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/vapi_integration_vapi_sdk.xml',
+        'views/vapi_log_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/res_users_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            '/polpo_vapi_integration/static/src/js/vapi_integration_widget.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
---
+++ polpo_vapi_integration/models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_users
+from . import vapi_log
+from . import res_config_settings
+
