15b5d952e1b2f2324761ca7e3455f3cc2b69beaa
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-09-20 13:33:41 -0300

    [FIX] ai_oca_bridge_mrp: change icon and fix name and category - manifest

--- ai_oca_bridge_mrp/__manifest__.py
+++ ai_oca_bridge_mrp/__manifest__.py
@@ -2,12 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Ai Oca Bridge MRP",
+    "name": "AI OCA Bridge MRP",
     "summary": """Adds MRP triggers for AI Bridges""",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "maintainers": ["marcelsavegnago"],
+    "category": "AI",
     "website": "https://github.com/OCA/ai",
     "depends": [
         "ai_oca_bridge",
