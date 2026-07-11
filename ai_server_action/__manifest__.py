bafde71c8e0c169f2cf138a74beb8eefb1ce830a
amoya <amoyapardo@gmail.com>
2026-07-11 09:50:55 +0200

    [MIG] ai_automation: migration to 18.0

--- ai_server_action/__manifest__.py
+++ ai_server_action/__manifest__.py
@@ -3,19 +3,16 @@
 
 {
     "name": "Ai Automation",
-    "summary": """Integrate `ai_tools` with server actions to automate tasks using AI.""",
-    "version": "16.0.1.0.0",
+    "summary": """
+    Integrate AI connections with server actions to automate tasks using LLMs.
+    """,
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/ai",
-    "depends": ["ai_tool"],
-    "external_dependencies": {
-        "python": ["ollama"],
-    },
+    "depends": ["ai_connection", "ai_tool"],
     "data": [
         "views/ir_actions_server.xml",
-        "security/ir.model.access.csv",
-        "views/ai_connection.xml",
     ],
     "demo": [],
 }
